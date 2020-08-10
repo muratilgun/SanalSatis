7183e0d05eaddcd0436c141357855fd2935b14d1
Murat Ilgun <muratilgun34@hotmail.com>
2020-08-10 13:01:45 +0300

    Repo düzeltildi.

--- SanalSatis.Infrastructure/DataAccess/ProductRepository.cs
+++ SanalSatis.Infrastructure/DataAccess/ProductRepository.cs
@@ -16,11 +16,17 @@ namespace SanalSatis.Infrastructure.DataAccess
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .FirstOrDefaultAsync(p => p.Id == id);
         }
         public async Task<IReadOnlyList<Product>> GetProductAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
