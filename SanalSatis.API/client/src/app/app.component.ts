1f71fd8ee0a42856a688ccda8d008754615c1d30
Murat Ilgun <muratilgun34@hotmail.com>
2020-08-17 23:46:37 +0300

    arayüz oluşturuldu

--- SanalSatis.API/client/src/app/app.component.ts
+++ SanalSatis.API/client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {IProduct} from './models/product';
-import {IPagination} from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -10,13 +8,7 @@ import {IPagination} from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Sanal Satis';
-  products: IProduct[];
-  constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-  }, error => {
-    console.log(error);
-  });
-  }
+
+  constructor() {}
+  ngOnInit(): void { }
 }
---
+++ SanalSatis.API/client/src/app/shop/shop.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { IPagination } from '../shared/models/pagination';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
+import {map, delay} from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+
+  // tslint:disable-next-line: typedef
+  getProducts(shopParams: ShopParams ) {
+    let params = new HttpParams();
+    if (shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if (shopParams.typeId !== 0) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+      // tslint:disable-next-line: align
+      params = params.append('sort', shopParams.sort);
+      // tslint:disable-next-line: align
+      params = params.append('pageIndex', shopParams.pageNumber.toString());
+      // tslint:disable-next-line: align
+      params = params.append('pageIndex', shopParams.pageSize.toString());
+
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  // tslint:disable-next-line: typedef
+  getBrands() {
+  return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  // tslint:disable-next-line: typedef
+  getTypes() {
+  return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+
+}
+
